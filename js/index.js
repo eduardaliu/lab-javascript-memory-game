8c987a2d27924c3b3430afc29cf1e4abd3aff94a
Eduarda Liu <eduardaliu@gmail.com>
2020-06-13 21:52:13 +0200

    done

--- js/index.js
+++ js/index.js
@@ -26,24 +26,40 @@ const cards = [
 ];
 
 const memoryGame = new MemoryGame(cards);
+memoryGame.shuffleCards();
 
-window.addEventListener('load', event => {
+let turnedCards = [];
+
+addEventListener('load', event => { // window.addEventListener, mas não precisa escreve window - loop pra definir cards
   let html = '';
   memoryGame.cards.forEach(pic => {
-    html += `<div class="card" data-card-name="${pic.name}">`;
-    html += `<div class="back" name="${pic.img}"></div>`;
-    html += `<div class="front" style="background: url(img/${pic.img}) no-repeat"></div>`;
-    html += `</div>`;
+    html += `<div class="card" card-name="${pic.name}">
+                <div class="back" name="${pic.img}"></div>
+                <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
+             </div>`;
   });
 
-  // Add all the divs to the HTML
+  // add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
 
-  // Bind the click event of each element to a function
+  // bind the click event of each element to a function 
   document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+    card.addEventListener('click', (card) => {
+        card.target.parentNode.classList.add("turned"); // vira o card que o loop ta iterando sobre
+        turnedCards.push(card.target.parentNode) // bota esse card no array de turnedcards
+        if (turnedCards.length == 2) { // se dois cards tão virados
+          setTimeout(() => {
+            if (memoryGame.checkIfPair(turnedCards[0].getAttribute("card-name"), turnedCards[1].getAttribute("card-name")) == false) { // se não tiver o mesmo nome, remove o turn
+              turnedCards[0].classList.remove("turned");
+              turnedCards[1].classList.remove("turned");
+            }
+            turnedCards = [] // empties the array
+            document.querySelector("#pairs-clicked").innerHTML = memoryGame.pairsClicked; // putting the numbers
+            document.querySelector("#pairs-guessed").innerHTML = memoryGame.pairsGuessed;
+            if (memoryGame.isFinished()) alert('YASSS');
+          }, 500) // delay pra virar de volta
+      }
     });
   });
-});
+
+})
--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,32 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    if (!this.cards) return undefined;
+    let arrayLength = this.cards.length;
+    let randomElement;
+    let lastElement;  
+    while (arrayLength) {     // While there remain elements to shuffle…
+      randomElement = Math.floor(Math.random() * arrayLength--);         // Pick a remaining element
+      lastElement = this.cards[arrayLength];        // And swap it with the current element.
+      this.cards[arrayLength] = this.cards[randomElement];
+      this.cards[randomElement] = lastElement;
+    }
+    // return this.cards;
+   }
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1;
+    if (card1 == card2) {
+      this.pairsGuessed += 1;
+      return true;
+    } else if (card1 != card2) return false;
+  }
+  isFinished() {
+    if (this.pairsGuessed == this.cards.length / 2 ) return true;
+    else return false;
+  }
 }
